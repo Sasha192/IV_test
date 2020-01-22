5964d152c38d0453d37608f6bcaac157d95e9b3a
Sasha192 <sasha192.bunin@gmail.com>
2020-01-23 00:05:11 +0200

    gg

--- Solution.java
+++ Solution.java
@@ -2,16 +2,40 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
-import java.util.TreeSet;
 
 public class Solution {
     private static final String FILE_PATH;
 
     static {
         FILE_PATH = "wordsforproblem.txt";
+    }
+
+    public List<String> solve(List<String> words) {
+        Set<String> set = new HashSet<>();
+        int minLength = Integer.MAX_VALUE;
+        for(String word : words) {
+            if(word.length() == 0) {
+                continue;
+            }
+            set.add(word);
+            if(word.length() < minLength) minLength = word.length();
+        }
+        List<String> concatenated = new ArrayList<>();
+        for (String word : words) {
+            if (word.length() != 0 && isConcatenated(word, set, minLength, 0)) {
+                concatenated.add(word);
+            }
+        }
+        concatenated.sort((arg1, arg2) -> {
+            return arg2.length() - arg1.length();
+        });
+        printResults(concatenated);
+        return concatenated;
     }
 
     private boolean isConcatenated(String word, Set<String> set, int minLength, int numberUnits) {
@@ -29,40 +53,19 @@ public class Solution {
         return false;
     }
 
-    public void solve(List<String> words) {
-        Set<String> set = new HashSet<>();
-        int minLength = Integer.MAX_VALUE;
-        for(String word : words) {
-            if(word.length() == 0) {
-                continue;
-            }
-            set.add(word);
-            if(word.length() < minLength) minLength = word.length();
-        }
-        TreeSet<String> concatenated = new TreeSet<>((first, second) -> {
-        	return first.length() - second.length();
-        });
-        for (String word : words) {
-            if (word.length() != 0 && isConcatenated(word, set, minLength, 0)) {
-                concatenated.add(word);
-            }
-        }
-        printResults(concatenated);
-    }
-
-    private static void printResults(TreeSet<String> results) {
+    private static void printResults(List<String> results) {
         int size = results.size();
-        System.out.println(results.pollLast());
-        System.out.println(results.pollLast());
+        System.out.println(results.get(0));
+        System.out.println(results.get(1));
         System.out.println(size);
     }
 
     public static void main(String[] args) {
         List<String> words = new ArrayList<>();
         try(BufferedReader reader = new BufferedReader(new FileReader(FILE_PATH))) {
-        	while(reader.ready()) {
-         	   words.add(reader.readLine());
-        	}
+            while(reader.ready()) {
+                words.add(reader.readLine());
+            }
         } catch (IOException exception) {
             exception.printStackTrace();
         }
