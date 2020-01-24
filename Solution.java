8bf7a5c876b20f2ae8601b2eff7c7ed96c99d405
Sasha192 <sasha192.bunin@gmail.com>
2020-01-24 17:41:05 +0200

    fixed issues

--- Solution.java
+++ Solution.java
@@ -2,43 +2,42 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 
 public class Solution {
-    private static final String FILE_PATH;
+	// I decided to write code, as i solve problems on HackerRank, CodeWars, LeetCode : thinking about ckecking my problem-solving skills;
+	// I don't know, how you check my work : manually ( with help of terminal "javac ...") or automated.
+    // So, if it is needed : i can write it is with Maven + SOLID + more flexibility
 
-    static {
-        FILE_PATH = "wordsforproblem.txt";
-    }
+    private static final String FILE_PATH = "wordsforproblem.txt";
 
-    public List<String> solve(List<String> words) {
+    public List<String> findConcatenatedWords(List<String> words) {
         Set<String> set = new HashSet<>();
-        int minLength = Integer.MAX_VALUE;
-        for(String word : words) {
-            if(word.length() == 0) {
+        int minLength = Integer.MAX_VALUE; // e.g. we have minLength word = 15, hence checking isConcatenatedWord should started from minLength
+        for (String word : words) {
+            if (word.length() == 0) {
                 continue;
             }
             set.add(word);
-            if(word.length() < minLength) minLength = word.length();
+            if (word.length() < minLength) {
+                minLength = word.length();
+            }
         }
         List<String> concatenated = new ArrayList<>();
         for (String word : words) {
-            if (word.length() != 0 && isConcatenated(word, set, minLength, 0)) {
+            if (word.length() != 0 && isConcatenatedWord(word, set, minLength, 0)) {
                 concatenated.add(word);
             }
         }
         concatenated.sort((arg1, arg2) -> {
             return arg2.length() - arg1.length();
         });
-        printResults(concatenated);
         return concatenated;
     }
 
-    private boolean isConcatenated(String word, Set<String> set, int minLength, int numberUnits) {
+    private boolean isConcatenatedWord(String word, Set<String> set, int minLength, int numberUnits) {
         if (numberUnits > 0
                 && set.contains(word)) {
             return true;
@@ -46,11 +45,25 @@ public class Solution {
         int stringLength = word.length();
         for (int i = minLength; i < stringLength; i++) {
             if (set.contains(word.substring(0, i))
-                    && isConcatenated(word.substring(i), set, minLength, numberUnits + 1)) {
+                    && isConcatenatedWord(word.substring(i), set, minLength, numberUnits + 1)) {
                 return true;
             }
         }
         return false;
+    }
+
+    public static void main(String[] args) {
+        List<String> words = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(FILE_PATH))) {
+            while (reader.ready()) {
+                words.add(reader.readLine());
+            }
+        } catch (IOException exception) {
+            throw new RuntimeException("Can't read file from " + FILE_PATH);
+        }
+        Solution solution = new Solution();
+        List<String> allSortedConcatenatedWords = solution.findConcatenatedWords(words);
+        printResults(allSortedConcatenatedWords);
     }
 
     private static void printResults(List<String> results) {
@@ -58,18 +71,5 @@ public class Solution {
         System.out.println(results.get(0));
         System.out.println(results.get(1));
         System.out.println(size);
-    }
-
-    public static void main(String[] args) {
-        List<String> words = new ArrayList<>();
-        try(BufferedReader reader = new BufferedReader(new FileReader(FILE_PATH))) {
-            while(reader.ready()) {
-                words.add(reader.readLine());
-            }
-        } catch (IOException exception) {
-            exception.printStackTrace();
-        }
-        Solution solution = new Solution();
-        solution.solve(words);
     }
 }
